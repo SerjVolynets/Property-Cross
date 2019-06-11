2055106a05dc205d748bbfe109cbe13131cb1e68
Serj <sergius.volynets@gmail.com>
2019-06-11 17:32:18 +0300

    trying module

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -2,7 +2,8 @@ var input = document.querySelector('input');
 var form = document.querySelector('form');
 var loc;
 var jsonString;
-var  objRequest;
+var objRequest;
+
 
 // console.log(divRoot);
 form.onsubmit = function (e) {
@@ -25,9 +26,12 @@ function sendRequest () {
     console.log(request.responseText)
         jsonString = request.responseText;
         objRequest = JSON.parse(jsonString);
-        
+
         console.log(objRequest);
         console.log(objRequest.response.listings[0].img_url);
+        
+        window.open('resultPage.html');
+        
         // drawResult(objRequest);
         
         
@@ -37,10 +41,9 @@ function sendRequest () {
 request.send();
 
 }
-
-
+export {objRequest};
 // function drawResult (objRequest){
-//     window.open('resultPage.html');
+    
 //     var divBox = document.createElement('div');
 //     var img = document.createElement('img');
 //     var price = document.createElement('p');
--- static/scripts/result.js
+++ static/scripts/result.js
@@ -1,16 +1,21 @@
-
-// function drawResult (objRequest){
+import {objRequest} from './main.mjs/index.js';
+window.onload = function () {
     
-//     var divBox = document.createElement('div');
-//     var img = document.createElement('img');
-//     var price = document.createElement('p');
-//     var description = document.createElement('p');
+    function drawResult (value){
+    
+        var divBox = document.createElement('div');
+        var img = document.createElement('img');
+        var price = document.createElement('p');
+        var description = document.createElement('p');
+        
+        
+        
+        img.setAttribute('src', value.response.listings[0].img_url)
+        var divRoot = document.getElementById('root');
+        divRoot.appendChild(img);
+    }
     
     
+    drawResult(objRequest);
     
-//     img.setAttribute('src',objRequest.response.listings[0].img_url)
-//     var divRoot = document.getElementById('root');
-//     divRoot.appendChild(img);
-// }
-
-// drawResult(objRequest);
+}
