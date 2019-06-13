fca64c14933b559cbf3e50e07bc55e22c9214b41
Serj <sergius.volynets@gmail.com>
2019-06-13 18:36:32 +0300

    react components

--- src/App.js
+++ src/App.js
@@ -1,1 +1,16 @@
-import React from 'react';
+import React, {Component} from 'react';
+import Search from './components/search.js'
+
+
+
+class App extends Component {
+    render () {
+        return <div>
+                    <h1>Property Cross in UK</h1>
+                    <p>Use the form below to search for houses to buy. You can search by place-name or postcode.</p>
+                    <Search />
+                </div>
+    }
+}
+
+export default App;
